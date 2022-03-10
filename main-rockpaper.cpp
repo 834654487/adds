9b574268ad7cb576babfd8583623e9b346805cd9
834654487 <79827639+834654487@users.noreply.github.com>
2022-03-10 11:22:03 +0800

    Add files via upload

---
+++ main-rockpaper.cpp
@@ -0,0 +1,15 @@
+#include"Referee.h"
+#include"Human.h"
+#include"Computer.h"
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    Referee game1;
+    game1.play();
+
+
+    return 0;
+
+}
