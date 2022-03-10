b21adde4ede5264666c24f5794d2997925577932
834654487 <79827639+834654487@users.noreply.github.com>
2022-03-10 10:55:56 +0800

    Add files via upload

---
+++ Computer.h
@@ -0,0 +1,11 @@
+#ifndef COMPUTER_H
+#define COMPUTER_H
+#include<iostream>
+#include<string>
+using namespace std;
+class Computer{
+    public:
+    Computer();
+    char makeMove();
+};
+#endif 
---
+++ main-rockpaper.cpp
@@ -0,0 +1,12 @@
+#include"Referee.h"
+#include"Human.h"
+#include"Computer.h"
+#include<iostream>
+#include<string>
+using namespace std;
+int main{
+    Referee game;
+    game.play();
+    return 0;
+    
+}
