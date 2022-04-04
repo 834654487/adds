6916187d048f0f884537698a21f4a2520fc1cb4c
834654487 <79827639+834654487@users.noreply.github.com>
2022-04-04 22:24:19 +0800

    Add files via upload

---
+++ Reverse.cpp
@@ -0,0 +1,32 @@
+#include"Reverse.h"
+#include<math.h>
+#include<iostream>
+#include<string>
+using namespace std;
+Reverse::Reverse(){
+    
+}
+int Reverse::reverseDigit(int value){
+    if(value < 10 ){
+        return value; 
+    }
+    int len = 0;
+    for(int i=value;i;i/=10){
+        len++;
+    }
+    return value%10*(int)pow(10, len-1)+reverseDigit(value/10);
+    
+}
+
+std::string Reverse::reverseString(std::string letters){
+    size_t characters=letters.size();
+    if(characters==1){
+        return letters;
+    }
+    else{
+        return letters[characters-1]+reverseString(letters.substr(0, characters-1));
+
+    }
+
+}
+
